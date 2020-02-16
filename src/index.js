72ec2e4eb47597a53f124c099c39a1bb43e139e1
Tarun215 <tarun.cer18@itbhu.ac.in>
2020-02-16 16:02:18 +0530

    added location sharing feature

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
+const http = require('http')
 const express = require('express')
 const path = require('path')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const server = http.createServer(app)
+const io = socketio(server)
 
+const PORT = process.env.PORT || 3000
 const publicDirPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirPath))
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('a new socket connection')
+
+    socket.emit('message', "Welcome!")
+    socket.broadcast.emit('message', 'A new user has joined')
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', message)
+        callback()
+    })
+
+    socket.on('sendLocation', (position, callback) => {
+        io.emit('message', `https://google.com/maps?q=${position.latitude},${position.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
+    })
+})
+
+server.listen(PORT, () => {
     console.log(`app is running at port ${PORT}`)
 })
