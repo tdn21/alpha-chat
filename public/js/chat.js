1cde8445b6247fa91862ae0d9e5969bd48473208
Tarun215 <tarun.cer18@itbhu.ac.in>
2020-02-17 20:12:43 +0530

    added autoscrolling

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,6 +15,29 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 //Options
 const { username , room } = Qs.parse(location.search, { ignoreQueryPrefix : true })
 
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    //Visible height
+    const visibleHeight = $messages.offsetHeight
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have the user scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -41,6 +64,7 @@ socket.on('message', ({ text, username, createdAt }) => {
         createdAt : moment(createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 //Location message socket
@@ -51,6 +75,7 @@ socket.on('locationMessage', ({ url, username, createdAt }) => {
             createdAt : moment(createdAt).format('h:mm a')
         })
         $messages.insertAdjacentHTML('beforeend', html)
+        autoscroll()
     })
 
 //roomData socket
